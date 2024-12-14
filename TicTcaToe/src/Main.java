c474f6a1dd1911c1398337a4a54e47c9b3377ad5
Ananya <120544737+ananya002muthu@users.noreply.github.com>
2024-12-14 12:59:10 +0530

    Add files via upload

---
+++ TicTcaToe/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TicTacToe tictactoe =new TicTacToe();
+		
+	}
+
+}
